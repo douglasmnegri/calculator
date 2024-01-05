3874b518f51f483398e5c25144e4c7883a30ea44
Douglas Melo <douglasmnegri@gmail.com>
2024-01-04 21:07:15 -0300

    The calculator display is now working. The next step should be to make use of the operate function. In the future, we should also swap the if/else statements, for an array containing the IDs/numbers of each div, and use the .map function to iterate the array, and display the correct number.

--- file.js
+++ file.js
@@ -72,3 +72,145 @@ container.appendChild(screenResult)
 
 createBoxes(4);
 console.log(operate());
+
+
+const square = document.querySelectorAll('.box');
+square.forEach((div, index) => {
+    const randomNumber = index + 1;
+    div.classList.add(`square${randomNumber}`);
+});
+
+
+
+
+const squareOne = document.querySelector('.square1');
+const squareTwo = document.querySelector('.square2');
+const squareThree = document.querySelector('.square3');
+const squareFour = document.querySelector('.square4');
+squareOne.textContent = "7";
+squareTwo.textContent = "8";
+squareThree.textContent = "9";
+squareFour.textContent = "+";
+
+
+const squareFive = document.querySelector('.square5');
+const squareSix = document.querySelector('.square6');
+const squareSeven = document.querySelector('.square7');
+const squareEight = document.querySelector('.square8'); 
+squareFive.textContent = "4";
+squareSix.textContent = "5";
+squareSeven.textContent = "6";
+squareEight.textContent = "-";
+
+const squareNine = document.querySelector('.square9');
+const squareTen = document.querySelector('.square10');
+const squareEleven = document.querySelector('.square11');
+const squareTwelve = document.querySelector('.square12'); 
+squareNine.textContent = "1";
+squareTen.textContent = "2";
+squareEleven.textContent = "3";
+squareTwelve.textContent = "x";
+
+const squareThirteen = document.querySelector('.square13');
+const squareFourteen = document.querySelector('.square14');
+const squareFifteen = document.querySelector('.square15');
+const squareSixteen = document.querySelector('.square16'); 
+squareThirteen.textContent = "C";
+squareFourteen.textContent = "0";
+squareFifteen.textContent = "=";
+squareSixteen.textContent = "÷";
+
+//We need to select each box, and add a function that process the number and shows it in our display.
+//addEventListener should be the function that calls each number/operations. 
+
+
+function selectNumber() {
+    for (let i = 1; i <= 16; i++) {
+    const box = document.querySelector(`.square${i}`);
+    box.addEventListener('click', () => {
+        
+    if(firstNumber === 0) {
+        if(box.classList.contains('square1')) {
+            firstNumber = 7;
+            console.log(firstNumber)
+
+        }
+       else if (box.classList.contains('square2')) {
+            firstNumber += 8;
+       }
+       else if (box.classList.contains('square3')) {
+            firstNumber += 9;
+       }
+       else if (box.classList.contains('square5')) {
+            firstNumber += 4;
+       }
+       else if (box.classList.contains('square6')) {
+            firstNumber += 5;
+       }
+       else if (box.classList.contains('square7')) {
+            firstNumber += 6;
+       }
+       else if (box.classList.contains('square9')) {
+            firstNumber += 1
+       }
+       else if (box.classList.contains('square10')) {
+            firstNumber += 2
+       }
+       else if (box.classList.contains('square11')) {
+            firstNumber += 3
+       }
+       screenResult.textContent = firstNumber;
+    }
+
+    else if (firstNumber !== 0) {
+        if(box.classList.contains('square1')) {
+            firstNumber = "7";
+
+        }
+       else if (box.classList.contains('square2')) {
+            firstNumber = "8";
+       }
+       else if (box.classList.contains('square3')) {
+            firstNumber = "9";
+       }
+       else if (box.classList.contains('square5')) {
+            firstNumber = "4";
+       }
+       else if (box.classList.contains('square6')) {
+            firstNumber = "5";
+       }
+       else if (box.classList.contains('square7')) {
+            firstNumber = "6";
+       }
+       else if (box.classList.contains('square9')) {
+        firstNumber = "1"
+       }
+        else if (box.classList.contains('square10')) {
+            firstNumber = "2"
+       }
+        else if (box.classList.contains('square11')) {
+            firstNumber = "3";
+       }
+       screenResult.textContent += firstNumber;
+    }
+    });
+};
+};
+
+selectNumber();
+//When the variable is called by the function addEventListener, it should show the value in our display.
+//The initial value will always be 0, so we can't simply concantenate for the first time. 
+
+/* Ex: if (screenResult.textContent === 0) {
+
+    switch (screenResult) {
+    case squareOne:
+    screenResult.textContent = 7;
+    break;
+
+    case squareTwo:
+    screenResult.textContent = 8;
+    break;
+    }
+} */
+squareSixteen.textContent = "÷";
